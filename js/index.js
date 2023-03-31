0226eaf185b81e5c36b55514213b4e52a8121184
Dawlet Kenesbaev <123051703+NUKUS777@users.noreply.github.com>
2023-03-31 07:26:15 -0700

    oo

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ const box = document.querySelector('.row');
 
 fetch('https://restcountries.com/v3.1/all').then((response)=> response.json()).then
 ((data) => {
-
   data.sort((a, b) => {
     if (a.name.common < b.name.common) {
       return -1;
@@ -18,29 +17,32 @@ fetch('https://restcountries.com/v3.1/all').then((response)=> response.json()).t
     }
   });
    const newData= data.map((country) =>{
+      
       let newpop = country.population.toString();
       let newi=[]
-      let ner=''
       if (newpop.length<=6) {
-       newi= newpop.slice(newpop.length-4,newpop.length-1)
-       ner = newi.split() ;
+       newi= `${parseInt(newpop.slice(0,newpop.length-3))/10}`
       }
-     else if (newpop.length>6) {
-        newi= newpop.slice(newpop.length-7,newpop.length-4)
-        ner = newi.split() ;
+     else if (newpop.length>6 && newpop.length<=9) {
+      newi= `${parseInt(newpop.slice(0,newpop.length-5))/10}m`
        }
+       else if(newpop.length>9){
+        newi= `${parseInt(newpop.slice(0,newpop.length-8))/10}b`
+       }
+    
+       console.log(newi);
       return ` <div class="col-lg-3 col-md-4 col-sm-6">
         <div class="card .bg-primary-subtle p-3">
             <img src="${country.flags.png}" class="card-img-top" style="height: 60%;>
             <div class="card-body">
-              <h5 class="card-title">${newpop}</h5>
+              <h5 class="card-title">${country.name.common}</h5>
               <h5 class="card-title">${country.capital}</h5>
-              <p class="card-text">${ner}</p>
+              <p class="card-text">${newi}</p>
             </div>
           </div>
     </div>`
     })
-    box.innerHTML = newData.join('');
+    box.innerHTML= newData.join('');
 
  
 });
